38ae217aa30129fbc70222331709e50b6b7cedb9
Cog <>
2020-10-13 19:34:38 -0500

    Quick patch to fix crash in 9.0.1

--- Modules/EditBox.lua
+++ Modules/EditBox.lua
@@ -424,14 +424,14 @@ end
 
 function mod:SetBackdrop()
 	for _, frame in ipairs(self.frames) do
-		frame:SetBackdrop({
-			bgFile = Media:Fetch("background", self.db.profile.background),
-			edgeFile = Media:Fetch("border", self.db.profile.border),
-			tile = true,
-			tileSize = self.db.profile.tileSize,
-			edgeSize = self.db.profile.edgeSize,
-			insets = {left = self.db.profile.inset, right = self.db.profile.inset, top = self.db.profile.inset, bottom = self.db.profile.inset}
-		})
+		-- frame:SetBackdrop({
+		-- 	bgFile = Media:Fetch("background", self.db.profile.background),
+		-- 	edgeFile = Media:Fetch("border", self.db.profile.border),
+		-- 	tile = true,
+		-- 	tileSize = self.db.profile.tileSize,
+		-- 	edgeSize = self.db.profile.edgeSize,
+		-- 	insets = {left = self.db.profile.inset, right = self.db.profile.inset, top = self.db.profile.inset, bottom = self.db.profile.inset}
+		-- })
 		local c = self.db.profile.backgroundColor
 		frame:SetBackdropColor(c.r, c.g, c.b, c.a)
 
