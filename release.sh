e6e2f359d1bd3b516365a375021de280bce9b28b
Cog <>
2020-10-14 14:29:24 -0500

    Add very simple release script

---
+++ release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# A very dumb release script, will look into auto-packaging later. Maybe.
+
+gitDir="chatter-patched"
+releaseDir="Chatter"
+releasePrefix="Chatter-1.6.0-patched-"
+
+# Various sanity checks
+
+dir=$(basename "$(pwd)")
+
+fatal(){
+	echo "$1"; exit 2
+}
+
+if [[ "$dir" != "$gitDir" ]]; then
+	fatal "Must run from $gitDir"
+fi
+
+releaseTag=$(git tag --points-at HEAD)
+
+if [[ "$releaseTag" == "" ]]; then
+	fatal "No git tag defined"
+fi
+
+cd ..
+
+test -d "$releaseDir" && fatal "$releaseDir folder exists"
+
+set -x
+
+# Cut release
+
+cp -r "$gitDir" "$releaseDir"
+rm -rf "$releaseDir/.git" "$releaseDir/release.sh"
+7z a -r "$releasePrefix$releaseTag.zip" "$releaseDir"
+rm -rf "$releaseDir"
